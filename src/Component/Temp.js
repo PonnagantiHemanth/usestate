fa7c8a598a1f4005101a125a17ff02ecbf047242
Your NamHemanth <hemanthponnaganti1@gmail.com>
2025-02-22 11:19:23 +0530

    Updated

--- src/Component/Temp.js
+++ src/Component/Temp.js
@@ -1,33 +1,70 @@
-import { useState ,useEffect} from "react";
+import {useState,useEffect} from 'react'
 
 const Temp = () => {
   return (
     <div>
-      <News />
+      
+      <Demo />
     </div>
   );
 };
 
 export default Temp;
 
-function News() {
-    const [articles, setArticles] = useState([]);
+// function Demo  () {
+//   const [count,setCount] = useState(0) 
+//   return (
+//     <div>
+//       <h1>Count : {count}</h1>
+//       <button onClick = {() => setCount(count+1)}>Click</button>
+//     </div>
+//   )
+// }
 
-    useEffect(() => {
-        fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=b0b58e2420e541ada6a94057f80a95e0")
-            .then(response => response.json())
-            .then(data => setArticles(data.articles));
-    }, []); // Empty array means it runs only once
+// function UserList() {
+//   const [users, setUsers] = useState([]);
 
-    return (
-        <div>
-            <h1>Latest News</h1>
-            {articles.map((article, index) => (
-                <div key={index}>
-                    <h2>{article.title}</h2>
-                    <p>{article.description}</p>
-                </div>
-            ))}
-        </div>
-    );
+//   useEffect(() => {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then(response => response.json())
+//       .then(data => setUsers(data));
+//   }, []); // Empty dependency array → Runs only once when component mounts
+
+//   return (
+//     <div>
+//       <h2>User List</h2>
+//       <ul>
+//         {users.map(user => (
+//           <li key={user.id}>{user.name}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+
+
+
+
+function Demo () {
+  const [users,setusers] = useState([])
+  useEffect(() => {
+    async function main() {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users")
+      const data = await response.json() 
+      setusers(data);
+    }
+    main()
+  },[])
+  return (
+    <div>
+      <ul>
+        {users.map((eachItem) => 
+        <li key = {eachItem.id}>{eachItem.name}</li>
+        
+        )}
+      </ul>
+    </div>
+  )
 }
+
